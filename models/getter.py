e52da0965c76339991bbc84f4cf603d7d2af382b
HM <hieumdd@gmail.com>
2022-01-19 22:19:58 +0700

    feat: Excluded current date from weekly

--- models/getter.py
+++ models/getter.py
@@ -29,7 +29,8 @@ def metric_daily_sum(field: str) -> Getter:
                     ON _cal.RawDate = b._DATA_DATE
                 )
                 WHERE
-                    _DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -8 DAY)
+                    RawDate >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND RawDate <> CURRENT_DATE()
                 GROUP BY 1
             ),
             base2 AS (
@@ -81,7 +82,8 @@ def metric_daily_div(nume: str, denom: str) -> Getter:
                     ON _cal.RawDate = b._DATA_DATE
                 )
                 WHERE
-                    _DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -8 DAY)
+                    RawDate >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND RawDate <> CURRENT_DATE()
                     AND ExternalCustomerId = {external_customer_id}
                 GROUP BY 1
             ),
@@ -160,10 +162,25 @@ def metric_weekly_sum(field: str) -> Getter:
         ),
         base3 AS (
             SELECT
-                (SELECT SUM(d0) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d7,
-                (SELECT SUM(d7) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d14,
-                (SELECT SUM(d0) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dtm,
-                (SELECT SUM(d_mom) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dlm,
+                (
+                    SELECT SUM(d0) FROM base2
+                    WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND Date <> CURRENT_DATE()
+                ) AS d7,
+                (
+                    SELECT SUM(d7) FROM base2
+                    WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND Date <> CURRENT_DATE()
+                ) AS d14,
+                (
+                    SELECT SUM(d0) FROM base2
+                    WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                    AND Date <> CURRENT_DATE()) AS dtm,
+                (
+                    SELECT SUM(d_mom) FROM base2
+                    WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                    AND Date <> CURRENT_DATE()
+                ) AS dlm,
         )
         SELECT
             SAFE_DIVIDE(d7-d14,d14) AS dw,
@@ -218,10 +235,26 @@ def metric_weekly_div(nume: str, denom: str) -> Getter:
         ),
         base3 AS (
             SELECT
-                (SELECT SUM(nume0) / SUM(NULLIF(denom0, 0)) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d7,
-                (SELECT SUM(nume7) / SUM(NULLIF(denom7, 0)) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d14,
-                (SELECT SUM(nume0) / SUM(NULLIF(denom0, 0)) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dtm,
-                (SELECT SUM(nume_mom) / SUM(NULLIF(denom_mom, 0)) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dlm,
+                (
+                    SELECT SUM(nume0) / SUM(NULLIF(denom0, 0)) FROM base2
+                    WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND Date <> CURRENT_DATE()
+                ) AS d7,
+                (
+                    SELECT SUM(nume7) / SUM(NULLIF(denom7, 0)) FROM base2
+                    WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND Date <> CURRENT_DATE()
+                ) AS d14,
+                (
+                    SELECT SUM(nume0) / SUM(NULLIF(denom0, 0)) FROM base2
+                    WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                    AND Date <> CURRENT_DATE()
+                ) AS dtm,
+                (
+                    SELECT SUM(nume_mom) / SUM(NULLIF(denom_mom, 0)) FROM base2
+                    WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                    AND Date <> CURRENT_DATE()
+                ) AS dlm,
         )
         SELECT
             SAFE_DIVIDE(d7-d14,d14) AS dw,
@@ -234,7 +267,8 @@ def metric_weekly_div(nume: str, denom: str) -> Getter:
 def underspent_accounts(days: int) -> Getter:
     return (
         lambda dataset, table_suffix, external_customer_id: f"""
-        WITH base AS (SELECT 
+            WITH base AS (
+                SELECT 
                     bs._DATA_DATE AS Date,
                     c.CampaignName,
                     b.BudgetName,
@@ -247,6 +281,7 @@ def underspent_accounts(days: int) -> Getter:
                 ON bs.AssociatedCampaignId = c.CampaignId
                 WHERE
                     bs._DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
+                    AND bs._DATA_DATE <> CURRENT_DATE()
                     AND bs.ExternalCustomerId = {external_customer_id}
                     AND b._DATA_DATE = b._LATEST_DATE
                     AND b.ExternalCustomerId = {external_customer_id}
@@ -286,7 +321,8 @@ def underspent_accounts(days: int) -> Getter:
 def underspent_budgets(days: int) -> Getter:
     return (
         lambda dataset, table_suffix, external_customer_id: f"""
-            WITH base AS (SELECT 
+            WITH base AS (
+                SELECT 
                     bs._DATA_DATE AS Date,
                     c.CampaignName,
                     b.BudgetName,
@@ -299,6 +335,7 @@ def underspent_budgets(days: int) -> Getter:
                 ON bs.AssociatedCampaignId = c.CampaignId
                 WHERE
                     bs._DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
+                    AND bs._DATA_DATE <> CURRENT_DATE()
                     AND bs.ExternalCustomerId = {external_customer_id}
                     AND b._DATA_DATE = b._LATEST_DATE
                     AND b.ExternalCustomerId = {external_customer_id}
@@ -320,7 +357,6 @@ def underspent_budgets(days: int) -> Getter:
                 SELECT
                     BudgetName,
                     ANY_VALUE(Campaigns) AS Campaigns,
-                    -- Date,
                     SUM(Cost),
                     SUM(Amount),
                     (SUM(Cost) - AVG(Amount)) / AVG(Amount) AS underspent
@@ -338,32 +374,6 @@ def underspent_budgets(days: int) -> Getter:
     )
 
 
-def gdn_placements(days: int) -> Getter:
-    return (
-        lambda dataset, table_suffix, external_customer_id: f"""
-            WITH base AS (
-                SELECT p.DisplayName, SUM(pcs.Conversions) AS Conversions
-            FROM {dataset}.PlacementConversionStats_{table_suffix} pcs
-            INNER JOIN {dataset}.Placement_{table_suffix} p
-                ON pcs.CampaignId = p.CampaignId
-                AND pcs.AdGroupId = p.AdGroupId
-            -- WHERE pcs._DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
-            -- AND p._DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
-            AND pcs.ExternalCustomerId = {external_customer_id}
-            GROUP BY 1
-            ),
-            base2 AS (
-                SELECT 
-                    DisplayName, Conversions,
-                    (SELECT AVG(Conversions) FROM base) AS avg_conversions,
-            FROM base
-            )
-        SELECT COUNT(*) FROM base2
-        WHERE (Conversions - avg_conversions) / avg_conversions > 0.5
-    """
-    )
-
-
 def potential_negative_search_terms(days: int) -> Getter:
     return (
         lambda dataset, table_suffix, external_customer_id: f"""
@@ -375,7 +385,7 @@ def potential_negative_search_terms(days: int) -> Getter:
                 FROM {dataset}.SearchQueryStats_{table_suffix}
                 WHERE
                     _DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
-                    -- AND (Clicks > 50 AND Conversions = 0)
+                    AND _DATA_DATE <> CURRENT_DATE()
                     AND ExternalCustomerId = {external_customer_id}
                 GROUP BY 1
             ),
@@ -395,10 +405,12 @@ def disapproved_ads(days: int) -> Getter:
             WITH base AS (
                 SELECT ARRAY_AGG(DISTINCT CreativeId) AS creatives
                 FROM {dataset}.Ad_{table_suffix}
-            WHERE _DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
-                AND CombinedApprovalStatus = "disapproved"
-                AND Status = "ENABLED"
-                AND ExternalCustomerId = {external_customer_id}
+                WHERE
+                    _DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -{days} DAY)
+                    AND _DATA_DATE <> CURRENT_DATE()
+                    AND CombinedApprovalStatus = "disapproved"
+                    AND Status = "ENABLED"
+                    AND ExternalCustomerId = {external_customer_id}
             )
             SELECT * FROM base WHERE ARRAY_LENGTH(creatives) > 0    
         """
@@ -429,10 +441,26 @@ def metric_sis() -> Getter:
             ),
             base3 AS (
                 SELECT
-                    (SELECT SUM(d0) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d7,
-                    (SELECT SUM(d7) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d14,
-                    (SELECT SUM(d0) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dtm,
-                    (SELECT SUM(d_mom) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dlm,
+                    (
+                        SELECT SUM(d0) FROM base2
+                        WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                        AND Date <> CURRENT_DATE()
+                    ) AS d7,
+                    (
+                        SELECT SUM(d7) FROM base2
+                        WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                        AND Date <> CURRENT_DATE()
+                    ) AS d14,
+                    (
+                        SELECT SUM(d0) FROM base2
+                        WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                        AND Date <> CURRENT_DATE()
+                    ) AS dtm,
+                    (
+                        SELECT SUM(d_mom) FROM base2
+                        WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                        AND Date <> CURRENT_DATE()
+                    ) AS dlm,
             )
             SELECT
                 SAFE_DIVIDE(d7-d14,d14) AS dw,
@@ -466,10 +494,26 @@ def metric_topsis() -> Getter:
             ),
             base3 AS (
                 SELECT
-                    (SELECT SUM(d0) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d7,
-                    (SELECT SUM(d7) FROM base2 WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)) AS d14,
-                    (SELECT SUM(d0) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dtm,
-                    (SELECT SUM(d_mom) FROM base2 WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)) AS dlm,
+                    (
+                        SELECT SUM(d0) FROM base2
+                        WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                        AND Date <> CURRENT_DATE()
+                    ) AS d7,
+                    (
+                        SELECT SUM(d7) FROM base2
+                        WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                        AND Date <> CURRENT_DATE()
+                    ) AS d14,
+                    (
+                        SELECT SUM(d0) FROM base2
+                        WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                        AND Date <> CURRENT_DATE()
+                    ) AS dtm,
+                    (
+                        SELECT SUM(d_mom) FROM base2
+                        WHERE Date >= DATE_TRUNC(CURRENT_DATE(), MONTH)
+                        AND Date <> CURRENT_DATE()
+                    ) AS dlm,
             )
             SELECT
                 SAFE_DIVIDE(d7-d14,d14) AS dw,
@@ -498,6 +542,7 @@ def ad_group_cpa() -> Getter:
                     AND ags._DATA_DATE = c._DATA_DATE
                 WHERE
                     ags._DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND ags._DATE_DATE <> CURRENT_DATE()
                     AND ags.ExternalCustomerId = {external_customer_id}
                 GROUP BY 1, 2
             ),
@@ -545,6 +590,7 @@ def keyword_cpa() -> Getter:
                     AND kws._DATA_DATE = c._DATA_DATE
                 WHERE
                     kws._DATA_DATE >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND kws._DATE_DATE <> CURRENT_DATE()
                     AND kws.ExternalCustomerId = {external_customer_id}
                 GROUP BY 1, 2, 3
             ),
@@ -612,7 +658,9 @@ def metric_performance(field: str) -> Getter:
                     AVG(d7) AS d7,
                     AVG(d30) AS d30,
                 FROM base3
-                WHERE Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                WHERE
+                    Date >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY)
+                    AND Date <> CURRENT_DATE()
                     AND d0 IS NOT NULL
                     AND d7 IS NOT NULL
                     AND d30 IS NOT NULL
